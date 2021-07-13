c282e3eabccc920f5d1b65cda6b73e77114145d5
Ramcas42 <ramsescastillo42@gmail.com>
2021-07-12 23:47:20 -0400

    getters and setters

--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,8 +1,9 @@
 function AutoPlay() {}
-
 AutoPlay.prototype.run = function(player) {
-    player.toggleMute();
-    player.play();
-}
+  if (!player.muted) {
+    player.muted = true;
+  }
+  player.play();
+};
 
 export default AutoPlay;
